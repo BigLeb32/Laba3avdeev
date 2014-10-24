20629550da396ec25ba7075715358a2786286b58
VsevolodAvdeev <seva90394@gmail.com>
2014-10-24 12:26:09 +0400

    5

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 int main()
 
 {
-    //This is study pogram
+    //This program is just an example
 
     double x,y;
     cout<<"Enter two numbers, pls!"<<endl;
     cin>>x>>y;
  cout<<"sum: "<<x+y<<endl;
- cout<<"dif: "<<x-y;
+ cout<<"dif: "<<x-y<<endl;
+ cout<<"mult: "<<x*y;
 }
